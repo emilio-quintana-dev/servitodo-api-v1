9d0d9fbdca2591fdde3e495ccea8de3f628f93f2
emilio-quintana-dev <emilioquintana90@gmail.com>
2020-08-05 15:48:36 -0500

    built end points for handling user sessions

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,9 +3,26 @@ class AuthController < ApplicationController
         user = User.find_by(email: params[:email])
 
         if user && user.authenticate(params[:password])
-            render json: { id: user.id, email: user.email }
+            payload = { user_id: user.id }
+            secret = 'secret'
+            token = JWT.encode(payload, secret, 'HS256')
+            render json: { id: user.id, email: user.email, token: token }
         else
             render json: { error: "Invalid Username or password" }
         end
+    end
+
+    def show
+        token = request.headers["Authorization"].split(' ').last
+        decoded_token = JWT.decode(token, 'secret', true, { algorithm:'HS256' })
+        id = decoded_token.first['user_id']
+
+        user = User.find(id)
+        if(user)
+            render json: { id: user.id, email: user.email }
+        else
+            render json: { error: 'Invalid Token'}
+        end
+        
     end
 end
